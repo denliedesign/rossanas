45ad5f0eb6f2cacd951cc6537f11dc77c1273e56
denliedesign <customdenlie@gmail.com>
2025-02-17 13:50:30 -0600

    reservation link

--- resources/views/_nav-links.blade.php
+++ resources/views/_nav-links.blade.php
@@ -5,6 +5,7 @@
 {{--        <div class="col"><a href="">EVENTS</a></div>--}}
         <div class="col"><a href="/menu">MENU</a></div>
         <div class="col"><a href="/about">ABOUT</a></div>
+        <div class="col"><a href="https://tables.toasttab.com/restaurants/fcac12b7-63b7-495a-b6e6-3afde757dfe8/findTime" target="_blank">RESERVATIONS</a></div>
         <div class="col"><a href="https://www.toasttab.com/rossanas-table" target="_blank"><span class="brand-button rounded shadow">ORDER HERE</span></a></div>
     </div>
 </div>
